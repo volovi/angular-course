03a71330283f74d7c07cab7788996bd56f02969a
Alexey Volovikov <volovi@gmail.com>
2016-10-24 01:59:48 +0300

    impl

---
+++ assignment4-solution/src/routes.js
@@ -0,0 +1,43 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // Set up UI states
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'src/menuapp/templates/home.template.html'
+    })
+
+    .state('categories', {
+      url: '/categories',
+      templateUrl: 'src/menuapp/templates/main-categories.template.html',
+      controller: 'CategoriesController as categoriesCtrl',
+      resolve: {
+        categories: ['MenuDataService', function (MenuDataService) {
+          return MenuDataService.getAllCategories();
+        }]
+      }
+    })
+
+    .state('items', {
+      url: '/items/{categoryShortName}',
+      templateUrl: 'src/menuapp/templates/main-items.template.html',
+      controller: 'ItemsController as itemsCtrl',
+      resolve: {
+        items: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
+          return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
+        }]
+      }
+    });
+}
+
+})();
